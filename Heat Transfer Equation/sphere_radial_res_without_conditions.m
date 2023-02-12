12ff5c6183c26549638ba212c963095fa5871daf
ArafunTamim <92660931+ArafunTamim@users.noreply.github.com>
2023-02-12 11:33:53 -0500

    Update sphere_radial_res_without_conditions.m

--- Heat Transfer Equation/sphere_radial_res_without_conditions.m
+++ Heat Transfer Equation/sphere_radial_res_without_conditions.m
@@ -7,7 +7,5 @@ clear
 
 syms T(r)                   %Defining symbolic variables T and r
 ode=diff(r^2*diff(T,r))==0; %Defining ODE
-
 TSol(r)=dsolve (ode);       %Solving ODE
-
 pretty(TSol)                %Make it look pretty :)
